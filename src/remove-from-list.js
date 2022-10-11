e7977da63a4fc82b6f3d2f1c0f71b2403fb47587
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2022-10-11 14:23:35 +0300

    feat: removeFromList solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let pointer = l;
+
+  while (pointer !== null) {
+    if (l.value === k) {
+      l = l.next;
+      pointer = l;
+    }
+
+    while (pointer.next?.value === k) {
+      pointer.next = pointer.next.next;
+    }
+
+    pointer = pointer.next;
+  }
+  
+  return l;
 }
 
 module.exports = {
